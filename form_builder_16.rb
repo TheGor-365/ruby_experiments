d02927917f2f58f6eeadbe997e53144214745cb0
Gor <7495500@gmail.com>
2023-08-11 16:03:26 +0300

    Add tag.rb

--- form_builder_16.rb
+++ form_builder_16.rb
@@ -1,93 +1,78 @@
 require 'active_support/all'
 
 module HexletCode
-  class Tag
-    def self.build(name, **attributes)
-      attributes = attributes.map { |attr, value| " #{attr}='#{value}'" }
-      tag = []
+  autoload(:Tag, "./tag.rb")
 
-      tag << "<#{name}"
-      tag << attributes.join
-      tag << '>' if !unpaired?(name)
-      tag << yield if block_given?
-      unpaired?(name) ? tag << '>' : tag << "</#{name}>"
-      tag.join
-    end
-
-    def self.unpaired?(tag)
-      unpaired = %w[ br hr img input meta area base col embed link param source track command keygen menuitem wbr ]
-      unpaired.include?(tag) ? true : false
-    end
-  end
-end
-
-# pp HexletCode::Tag.build('br')
-# pp HexletCode::Tag.build('img', src: 'path/to/image')
-# pp HexletCode::Tag.build('input', type: 'submit', value: 'Save')
-# pp HexletCode::Tag.build('label') { 'Email' }
-# pp HexletCode::Tag.build('label', for: 'email') { 'Email' }
-# pp HexletCode::Tag.build('div')
-#
-# puts
-# puts
-
-
-
-module HexletCode
   class << self
-    def form_for struct, url={}, &block
+    def form_for(struct, url = {}, &block)
       @params = struct.to_h
 
       if url.key?(:url)
         puts "<form action='#{url.fetch(:url)}' method='post'>"
-        print instance_eval(&block)
-        puts '</form>'
+        # instance_eval(&block)
+        # yield_self if block_given?
+        puts "</form>"
       else
         puts "<form action='#' method='post'>"
-        print instance_eval(&block)
-        puts '</form>'
+        # instance_eval(&block)
+        # yield_self if block_given?
+        puts "</form>"
       end
     end
 
-    def input param_name, **field_options
+    def input(param_name, **field_options)
       @params.merge! field_options
 
       @input_attrs = @params.each_with_object({}) do |(name, value), hash|
         case field_options[:as]
         when :text
-          hash[name] = "name='#{name}' cols='#{field_options.fetch(:cols, 20)}' rows='#{field_options.fetch(:rows, 40)}'"
+          hash[name] =
+            "name='#{name}' cols='#{field_options.fetch(:cols, 20)}' rows='#{field_options.fetch(:rows, 40)}'"
         else
           hash[name] = "name='#{name}' type='text' value='#{value}'"
         end
       end
-      field_constructor param_name, **field_options
-
-      puts @field.join
+      field_constructor(param_name)
     end
 
-    def submit *button_name
-      "  #{HexletCode::Tag.build('input', type: 'submit', value: button_name.present? ? button_name.join : 'Save')}\n"
+    def submit(*button_name)
+      submit = []
+
+      submit << "  <input type='submit'"
+      submit << " name='#{button_name.present? ? button_name.join : 'Save'}'"
+      submit << ">"
+      puts submit.join
     end
 
-    def field_constructor param_name, **field_options
-      public_send(param_name) if !@params[param_name]
+    def field_constructor(param_name, **field_options)
+      public_send(param_name) unless @params[param_name]
+      field = []
 
-      @field = []
       case field_options[:as]
       when :text
-        @field << "  #{HexletCode::Tag.build('label', for: param_name) { param_name.to_s.capitalize }}\n"
-        @field << '  <textarea '
-        @field << @input_attrs.fetch(param_name)
-        @field << '>'
-        @field << @params.fetch(param_name)
-        @field << '</textarea>'
+        field << label(param_name)
+        field << "  <textarea "
+        field << @input_attrs.fetch(param_name)
+        field << ">"
+        field << @params.fetch(param_name)
+        field << "</textarea>"
       else
-        @field << "  #{HexletCode::Tag.build('label', for: param_name) { param_name.to_s.capitalize }}\n"
-        @field << '  <input '
-        @field << @input_attrs.fetch(param_name)
-        field_options.map { |option_name, value| @field << " #{option_name}='#{value}'" }
-        @field << '>'
+        field << label(param_name)
+        field << "  <input "
+        field << @input_attrs.fetch(param_name)
+        field_options.map { |option_name, value| field << " #{option_name}='#{value}'" }
+        field << ">"
       end
+      puts field.join
+    end
+
+    def label(param_name)
+      label = []
+
+      label << "  <label for='#{param_name.to_s}'"
+      label << param_name.to_s.capitalize
+      label << "</label>\n"
+      label.join
     end
   end
 end
@@ -110,25 +95,25 @@ end
 
 
 
-HexletCode.form_for user, url: '#' do |f|
-  f.input :name, class: 'user-input'
-  f.input :job
-end
+# HexletCode.form_for user, url: '#' do |f|
+#   f.input :name, class: 'user-input'
+#   f.input :job
+# end
+#
+# # <form action="#" method="post">
+# #   <input name="name" type="text" value="rob" class="user-input">
+# #   <input name="job" type="text" value="hexlet">
+# # </form>
+#
+#
 
-# <form action="#" method="post">
-#   <input name="name" type="text" value="rob" class="user-input">
-#   <input name="job" type="text" value="hexlet">
-# </form>
-
-
-
-HexletCode.form_for user, url: '/users' do |f|
-  f.input :job, as: :text, rows: 50, cols: 50
-end
-
-# <form action="#" method="post">
-#   <textarea cols="50" rows="50" name="job">hexlet</textarea>
-# </form>
+# HexletCode.form_for user, url: '/users' do |f|
+#   f.input :job, as: :text, rows: 50, cols: 50
+# end
+#
+# # <form action="#" method="post">
+# #   <textarea cols="50" rows="50" name="job">hexlet</textarea>
+# # </form>
 
 
 
---
+++ tag.rb
@@ -0,0 +1,32 @@
+module HexletCode
+  class Tag
+    class << self
+      def build(name, **attributes)
+        attributes = attributes.map { |attr, value| " #{attr}='#{value}'" }
+        tag = []
+
+        tag << "<#{name}"
+        tag << attributes.join
+        tag << ">" unless unpaired?(name)
+        tag << yield if block_given?
+        tag << (unpaired?(name) ? ">" : "</#{name}>")
+        tag.join
+      end
+
+      def unpaired?(tag)
+        unpaired = %w[ br hr img input meta area base col embed link param source track command keygen menuitem wbr ]
+        unpaired.include?(tag) ? true : false
+      end
+    end
+  end
+end
+
+pp HexletCode::Tag.build('br')
+pp HexletCode::Tag.build('img', src: 'path/to/image')
+pp HexletCode::Tag.build('input', type: 'submit', value: 'Save')
+pp HexletCode::Tag.build('label') { 'Email' }
+pp HexletCode::Tag.build('label', for: 'email') { 'Email' }
+pp HexletCode::Tag.build('div')
+
+puts
+puts
