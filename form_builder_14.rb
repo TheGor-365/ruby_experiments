d3f01839262a887f6e052c0efdae34da512544fb
Gor <7495500@gmail.com>
2023-08-16 23:34:37 +0300

    Add form builder 17

--- form_builder_14.rb
+++ form_builder_14.rb
@@ -40,30 +40,33 @@ puts
 
 
 class ShoppingList
+  attr_reader :params
+  
   def self.items &block
     array = []
 
     array << "wow\n"
     array << yield_self(&block)
-    array << "wow\n"
-    array
+    array << "\nwow\n"
+    array.join
   end
 
   def self.add options
-    array = []
+    @params = []
 
     options.each_with_object({}) do |(name, value), hash|
-      array << "  #{value}\n" if name
+      @params << "  #{value}" if name
     end
 
-    array.join
+    pp @params
   end
 end
 
 
 list = ShoppingList.items do |item|
-  item.add three: 'three 3', four: 'four 4'
+  item.add one: 'one 1', two: 'two 2'
   item.add five: 'five 5', six: 'six 6'
 end
 
 puts list
+pp ShoppingList.items
---
+++ form_builder_17.rb
@@ -0,0 +1,9 @@
+class FormBuilder
+  def initialize params
+    @params = params
+  end
+
+  def form_for
+
+  end
+end
