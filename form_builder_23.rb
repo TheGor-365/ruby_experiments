e6a7c5338d4a7a6c7d925a9a4db756b8378c0776
Gor <7495500@gmail.com>
2023-12-12 21:02:08 +0300

    Create form_builder_23.rb

--- form_builder_23.rb
+++ form_builder_23.rb
@@ -3,73 +3,68 @@ require "active_support/all"
 # generates HTML forms
 module HexletCode
   def self.form_for(struct, url = {}, &block)
-    @params = struct.to_h
     form = []
 
     form << (url.key?(:url) ? "<form action='#{url.fetch(:url)}' method='post'>\n" : "<form action='#' method='post'>\n")
-    form << instance_eval(&block)
+    form << yield(struct)
     form << "\n</form>"
     form.join
   end
+end
 
+public
 
-  def self.input(param_name, **field_options)
-    @params.merge! field_options
+def input(param_name, **field_options)
+  @params = self.to_h.merge! field_options
 
-    @input_attrs = @params.each_with_object({}) do |(name, value), hash|
-      case field_options[:as]
-      when :text
-        hash[name] =
-          "name='#{name}' cols='#{field_options.fetch(:cols, 20)}' rows='#{field_options.fetch(:rows, 40)}'"
-      else
-        hash[name] = "name='#{name}' type='text' value='#{value}'"
-      end
-    end
-    field_constructor(param_name, **field_options)
-  end
-
-
-  def self.submit(*button_name)
-    submit = []
-
-    submit << "  <input type='submit'"
-    submit << " name='#{button_name.present? ? button_name.join : "Save"}'"
-    submit << ">"
-    submit.join
-  end
-
-
-  def self.field_constructor(param_name, **field_options)
-    public_send(param_name) unless @params[param_name]
-    field = []
-
+  @input_attrs = @params.each_with_object({}) do |(name, value), hash|
     case field_options[:as]
-    when :text
-      field << label(param_name)
-      field << "  <textarea "
-      field << @input_attrs.fetch(param_name)
-      field << ">"
-      field << @params.fetch(param_name)
-      field << "</textarea>"
-    else
-      field << label(param_name)
-      field << "  <input "
-      field << @input_attrs.fetch(param_name)
-      field_options.map { |option_name, value| field << " #{option_name}='#{value}'" }
-      field << ">"
+    when :text then hash[name] = "name='#{name}' cols='#{field_options.fetch(:cols, 20)}' rows='#{field_options.fetch(:rows, 40)}'"
+    else hash[name] = "name='#{name}' type='text' value='#{value}'"
     end
-    field.join
   end
+  field_constructor(param_name, **field_options)
+end
 
 
-  def self.label(param_name)
-    label = []
+def submit(*button_name)
+  submit = []
+  submit << "  <input type='submit'"
+  submit << " name='#{button_name.present? ? button_name.join : "Save"}'"
+  submit << '>'
+  submit.join
+end
 
-    label << "  <label for='#{param_name}'"
-    label << param_name.to_s.capitalize
-    label << "</label>\n"
-    label.join
-  end
+
+def field_constructor(param_name, **field_options)
+  public_send(param_name) unless @params[param_name]
+  field = []
+
+  case field_options[:as]
+  when :text
+    field << label(param_name)
+    field << '  <textarea '
+    field << @input_attrs.fetch(param_name)
+    field << '>'
+    field << @params.fetch(param_name)
+    field << '</textarea>'
+  else
+    field << label(param_name)
+    field << '  <input '
+    field << @input_attrs.fetch(param_name)
+    field_options.map { |option_name, value| field << " #{option_name}='#{value}'" }
+    field << '>'
+  end; field.join
+end
+
+
+def label(param_name)
+  label = []
+
+  label << "  <label for='#{param_name}'"
+  label << param_name.to_s.capitalize
+  label << "</label>\n"
+  label.join
 end
 
 
